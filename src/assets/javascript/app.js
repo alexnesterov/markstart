a45cc07a3ec0edd86f1174ced27e6572f866fdc7
Alex Nesterov <ialexnesterov@gmail.com>
2020-05-20 23:31:46 +0300

    Add global js folder

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -1,24 +1,5 @@
 // import here
-import svg4everybody from 'svg4everybody';
-
-// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
-// to the top of the file so if we used`import` to import Foundation,
-// it would execute earlier than we have assigned the global variable.
-// This is why we have to use CommonJS require() here since it doesn't
-// have the hoisting behavior.
-// window.jQuery = $;
-
-window.$window = $(window);
-window.$document = $(document);
-window.$body = $('body');
-window.windowWidth = $window.width();
-window.windowScroll = 0;
-window.windowLastScroll = 0;
-
-$window.on('load', function () {
-	$body.addClass('page_loaded');
-	svg4everybody();
-});
+import './global/base';
 
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
---
+++ src/assets/javascript/global/base.js
@@ -0,0 +1,32 @@
+import svg4everybody from 'svg4everybody';
+
+window.$window = $(window);
+window.$document = $(document);
+window.$body = $('body');
+window.windowWidth = $window.width();
+window.windowScroll = 0;
+window.windowLastScroll = 0;
+
+$window.on('load', function () {
+	$body.addClass('page_loaded');
+	svg4everybody();
+});
+
+$window.on('scroll', function() {
+	windowScroll = $window.scrollTop();
+	if (windowScroll > 40) {
+		$body.addClass('page_scrolled');
+	} else {
+		$body.removeClass('page_scrolled');
+	}
+
+	if (windowScroll > 40) {
+		if (windowScroll > windowLastScroll) {
+			$body.addClass('page_scroll-down');
+		} else if (windowScroll < windowLastScroll) {
+			$body.removeClass('page_scroll-down');
+		}
+	}
+
+	windowLastScroll = windowScroll;
+});
