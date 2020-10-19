2b6682db2c5f8e1a68d64bfe8e2346d737b45c49
Alex Nesterov <ialexnesterov@gmail.com>
2020-10-19 16:47:48 +0300

    Добавление функции загрузки изображений по тригеру

--- src/assets/javascript/global/lozadLazy.js
+++ src/assets/javascript/global/lozadLazy.js
@@ -1,11 +1,26 @@
 import 'intersection-observer';
 import lozad from 'lozad';
 
-const observer = lozad('.lazy', {
+window.lazy = lozad('.lazy', {
 	loaded: function(el) {
 		el.onload = function() {
 			el.classList.add('_is-loaded');
 		}
 	},
 });
-observer.observe();
+lazy.observe();
+
+lazy.loadImages = (selector) => {
+	const imagesAll = document.querySelectorAll(
+		selector
+	);
+
+	for (
+		let index = 0;
+		index < imagesAll.length;
+		index++
+	) {
+		const image = imagesAll[index];
+		lazy.triggerLoad(image);
+	}
+};
