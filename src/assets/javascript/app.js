75e16a9a8952415d23a891ede3e552a15a12789c
Alex Nesterov <ialexnesterov@gmail.com>
2021-07-03 15:19:40 +0300

    fix

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -2,18 +2,12 @@ import './global/objectFitImages';
 import './global/lazy';
 import './global/base';
 
-// If you want to pick and choose which modules to include, comment out the above and uncomment
-// the line below
-//import './lib/foundation-explicit-pieces';
-
-
 let loading = () => {
 	import(/* webpackChunkName: "components" */ './components').then(() => {
 		console.log('Ready!');
 	});
-}
+};
 
-if (window.addEventListener)
-	window.addEventListener('load', loading, false);
+if (window.addEventListener) window.addEventListener('load', loading, false);
 else if (window.attachEvent) window.attachEvent('onload', loading);
 else window.onload = loading;
